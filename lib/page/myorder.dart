1ecbaba3ae288b20ac4fc4ed757eb2bdc85f5e66
Zamahsyari <zamahsyari@pegipegi.com>
2020-01-03 17:59:22 +0700

    fix nees repaint

--- lib/page/myorder.dart
+++ lib/page/myorder.dart
@@ -9,133 +9,17 @@ class Myorder extends StatefulWidget {
 class _MyorderState extends State<Myorder> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: new Text(
-          "Your Food Cart",
-          style: TextStyle(
-            color: Colors.black,
-          ),
-        ),
-        backgroundColor: Colors.white,
-        elevation: 0.0,
-        centerTitle: true,
-      ),
-      body: Container(
+    return Container(
           width:300.0,
           height: 100.0,
-      child: ListView(
-        padding: EdgeInsets.symmetric(horizontal: 10.0),
-        scrollDirection: Axis.vertical,
-        children: <Widget>[
+          child: ListView(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            scrollDirection: Axis.vertical,
+            children: <Widget>[
           OrderCard(),
           OrderCard(),
         ],
           ),
-      ),
-      bottomNavigationBar: _buildTotalContainer(),
-    );
-  }
-
-  Widget _buildTotalContainer() {
-    return Container(
-      height: 220.0,
-      padding: EdgeInsets.only(left: 10.0, right: 10.0),
-      child: Column(
-        children: <Widget>[
-          Row(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text(
-                  "Subtotal",
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.bold),
-                ),
-              ]),
-          SizedBox(
-            height: 10.0,
-          ),
-          Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Text(
-                "Discount",
-                style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.bold),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 10.0,
-          ),
-          Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Text(
-                "Tax",
-                style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.bold),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 10.0,
-          ),
-          Divider(
-            height: 2.0,
-          ),
-          SizedBox(
-            height: 20.0,
-          ),
-          Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Text(
-                "Cart Total",
-                style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.bold),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 10.0,
-          ),
-          GestureDetector(
-            onTap: () {},
-            child: Container(
-              height: 50.0,
-              decoration: BoxDecoration(
-                color: Theme.of(context).primaryColor,
-                borderRadius: BorderRadius.circular(35.0),
-              ),
-              child: Center(
-                child: Text(
-                  "Proceed to Checkout",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18.0,
-                      fontWeight: FontWeight.bold),
-                ),
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 20.0,
-          )
-        ],
-      ),
-    );
+      );
   }
 }
